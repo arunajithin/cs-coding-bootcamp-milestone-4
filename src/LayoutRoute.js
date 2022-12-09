8ac1241be3567a3ab423a7d3eac9bfe40c6390aa
aruna jithin <116826164+arunajithin@users.noreply.github.com>
2022-12-09 08:22:20 +0400

    footer and fonts added

--- src/LayoutRoute.js
+++ src/LayoutRoute.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import ResponsiveHeader from './ResponsiveHeader';
+import Footer from './Footer';
 
 function LayoutRoute(props){
     return(
         <React.Fragment>
             <ResponsiveHeader {...props}/>
             <Route path={props.path} exact={props.exact} component={props.component}/>
+            <Footer/>
         </React.Fragment>
     )
 }
