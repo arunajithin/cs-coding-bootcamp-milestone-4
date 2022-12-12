10b3bf4b5aa5f6ecc9a529d666f1810aa3b1810f
Abrima <abdullah.rimawi95@gmail.com>
2022-12-12 13:18:01 +0400

    Navbar and pages with paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './components/HomeScreen.js';
 import LayoutRoute from './LayoutRoute';
+import FruitsVegetables from './components/FruitsVegetables.js';
+import BakedProducts from './components/BakedProducts.js';
+import DairyEggs from './components/DairyEggs';
+import MeatPoultry from './components/MeatPoultry.js';
 
 function App() {
   return (
       <BrowserRouter>
         <Switch>
           <LayoutRoute path='/' exact={true} component={HomeScreen}/>
+          <LayoutRoute path='/components/fruitsVeg' exact={true} component={FruitsVegetables}/>
+          <LayoutRoute path='/components/bakedProducts' exact={true} component={BakedProducts}/>
+          <LayoutRoute path='/components/dairyEggs' exact={true} component={DairyEggs}/>
+          <LayoutRoute path='/components/meatPoultry' exact={true} component={MeatPoultry}/>
         </Switch>
       </BrowserRouter>
   );
