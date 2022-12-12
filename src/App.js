0f4e48ab260af956e9570c73c15da319985c0967
aruna jithin <116826164+arunajithin@users.noreply.github.com>
2022-12-12 22:16:35 +0400

    user login,registration and logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen.js';
-import LayoutRoute from './LayoutRoute';
 import FruitsVegetables from './components/FruitsVegetables.js';
 import BakedProducts from './components/BakedProducts.js';
 import DairyEggs from './components/DairyEggs';
 import MeatPoultry from './components/MeatPoultry.js';
+import RegistrationScreen from './RegistrationScreen';
+import LoginScreen from './LoginScreen';
+import LayoutRoute from './LayoutRoute';
+import GuestLayoutRoute from './GuestLayoutRoute';
+import PrivateLayoutRoute from './PrivateLayoutRoute';
 
 function App() {
   return (
@@ -16,6 +20,8 @@ function App() {
           <LayoutRoute path='/components/bakedProducts' exact={true} component={BakedProducts}/>
           <LayoutRoute path='/components/dairyEggs' exact={true} component={DairyEggs}/>
           <LayoutRoute path='/components/meatPoultry' exact={true} component={MeatPoultry}/>
+          <GuestLayoutRoute path="/register" exact={true} component={RegistrationScreen} />
+          <GuestLayoutRoute path="/login" exact={true} component={LoginScreen} />
         </Switch>
       </BrowserRouter>
   );
