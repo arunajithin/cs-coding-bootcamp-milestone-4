fc26af3578b912b95b4cb42490e7c17672231ff7
Emmanuel-UC <59890752+Emmanuel-UC@users.noreply.github.com>
2022-12-25 20:13:21 +0400

    profile screen code update

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -1,45 +1,79 @@
-import Box from '@mui/material/Box';
-import { useEffect, useContext, useState } from "react";
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import UserContext from "./UserContext.js";
-import { Avatar, FormControl, TextField } from '@mui/material';
+import { useEffect, useContext, useState} from "react";
+import {UserContext} from './UserContext.js';
 import Alert from '@mui/material/Alert';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
+import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { Redirect, Route } from 'react-router-dom';
+import { Checkbox } from "@mui/material";
+
 
 function ProfileScreen() {
 
- 
 
 
+    const [ userDetails, setUserDetails ] = useState();
+    const { jsonwebtoken, updateUser } = useContext(UserContext);
 
-   const borderRadius= {
-    "border-radius": "50%",
-    "left": "22%"
-   }
-
-  const [userDetails, setUserDetails] = useState();
-
-
-
-    // The states are: 
-    // (1) null, (2) "client error", (3) "loading", (4) "backend error", (5) "success"
     var [formState, setFormState] = useState(null);
     var [errorsState, setErrorsState] = useState([]);
+    var [changePassword, setChangePassword] = useState(false);
 
-
-    // 1. Declare variables (not defined)
     var firstNameField;
     var lastNameField;
     var emailField;
     var passwordField;
     var avatarField;
+    var passwordRadio;
 
-     
-    // Create a JS object like an HTML form element 
+    useEffect(
+        function() {
+
+            fetch(
+                `${process.env.REACT_APP_BACKEND_ENDPOINT}/users/find`,
+                {
+                    'method': 'POST',
+                    'headers': {
+                        'Authorization': `Bearer ${localStorage.getItem('jsonwebtoken')}`
+                    },
+                    // 'body': {}
+                }
+            )
+            // This will recieve string data and convert to json
+            .then(
+                function(backendReponse) {
+                    return backendReponse.json()
+                }
+            )
+            // This will receie the converted json
+            .then(
+                function(jsonResponse) {
+                    setUserDetails(jsonResponse);
+                    updateUser(
+                        {
+                            jsonwebtoken,
+                            ...jsonResponse,
+                        }
+                    )
+                }
+            )
+            // This will catch errors if any
+            .catch(
+                function(backendError) {
+                    console.log('backendError', backendError)
+                }
+            )
+        },
+
+        // This array is empty because useEffect will run once only
+        []
+    );
+
+
     var formData = new FormData();
 
     function attachFile(evt) {
@@ -54,27 +88,17 @@ function ProfileScreen() {
             }
         )
     }
-
-
-    function updateHandler() {
-
+    
+    function update() {
 
         // 2. Validate the fields
         var errors = [];
-
-        if(firstNameField.value.length === 0) {
-            errors.push('Please enter your first name');
-        }
-
-        if(lastNameField.value.length === 0) {
-            errors.push('Please enter your last name');
-        }
 
         if(emailField.value.length === 0) {
             errors.push('Please enter your email');
         }
 
-        if(passwordField.value.length === 0) {
+        if(passwordField.value.length === 0 && changePassword === true) {
             errors.push('Please enter your password');
         }
 
@@ -94,18 +118,16 @@ function ProfileScreen() {
             formData.append('firstName', firstNameField.value);
             formData.append('lastName', lastNameField.value);
             formData.append('email', emailField.value);
-            formData.append('password', passwordField.value);
-            formData.append('avatar', avatarField.value);
+            if (changePassword) {
+                formData.append('password', passwordField.value);
+            }
 
             fetch(
                 `${process.env.REACT_APP_BACKEND_ENDPOINT}/users/update`,
                 {
                     'method': 'PUT',
-                    'body': formData,
-                    headers: {
-                      Authorization: `Bearer ${localStorage.getItem("jsonwebtoken")}`
+                    'body': formData
                 }
-              }
             )
             .then(
                 function(backendResponse) {
@@ -119,10 +141,17 @@ function ProfileScreen() {
                     if(jsonResponse.status === "ok") {
                         console.log('backend response /users/update', jsonResponse)
                         setFormState("success");
+
+                        // Update the user context
+                        updateUser(
+                            {
+                                ...userDetails,
+                                ...jsonResponse
+                            }
+                        )
                     }
                     else {
                         setFormState("backend error");
-                        setErrorsState([jsonResponse.message]);
                     }
                 }
             )
@@ -136,176 +165,141 @@ function ProfileScreen() {
         }
     }
 
+
+    function togglePasswordChange(event) {
+       setChangePassword(event.currentTarget.checked);
+    }
+
     function addListItem(str) {
         return <li>{str}</li>
     }
 
+    if( userDetails ) {
+        const User = userDetails.firstName+" "+userDetails.lastName;
+        
+        return (
+            <Container maxWidth="sm">
+                 <Box pt={5} >
+                    <Typography component="text" fontSize="330%">
+                        Welcome {User}
+                    </Typography>
+                </Box>
 
+                <Box mt={4} mb={4}>
 
+                    <Box display="flex" justifyContent="center" flexDirection="column">
+                        <Avatar alt='Profile Picture' 
+                        src={userDetails.avatar} 
+                        sx={{width: 256, height: 256, margin: '0 auto'}}/> 
 
-
-  useEffect(
-    function () {
-      fetch(`${process.env.REACT_APP_BACKEND_ENDPOINT}/users/find`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("jsonwebtoken")}`,
-        },
-        // 'body': {}
-      })
-        // This will recieve string data and convert to json
-        .then(function (backendReponse) {
-          return backendReponse.json();
-        })
-        // This will receie the converted json
-        .then(function (jsonResponse) {
-          setUserDetails(jsonResponse);
-        })
-        // This will catch errors if any
-        .catch(function (backendError) {
-          console.log("backendError", backendError);
-        });
-    },
-
-    // This array is empty because useEffect will run once only
-    []
-  );
-
-  if (userDetails) {
-
-  const User = userDetails.firstName+" "+userDetails.lastName;
-
-    return (
-
-      <Container maxWidth="sm">
-      
-            <Box pt={5} >
-                <Typography component="text" fontSize="36px">
-                    Welcome {User}
-                </Typography>
-            </Box>
-
-            <Box pt={5}> 
-           
-      
-       <Avatar src={userDetails.avatar} style={borderRadius} alt="profile" sx={{ width: 300, height:300 }}/>
-  
-            </Box>
-
-            <Box mt={4} mb={2}>
-                <FormControl fullWidth sx={ { mb: 2 } }>
-                    <TextField 
-                    inputRef={ 
-                        function( thisElement ){
-                            firstNameField = thisElement;
-                        } 
-                    }
-                    label="First Name" required={true}/>
-                </FormControl>
-
-                <FormControl fullWidth sx={{ mb: 2 }}>
-                   <TextField 
-                   inputRef={ 
-                        function( thisElement ){
-                            lastNameField = thisElement;
-                        } 
-                    }
-                   label="Last Name" required={true}/>
-                </FormControl>
-
-                <FormControl fullWidth sx={{ mb: 2 }}>
-                    <TextField 
-                    inputRef={ 
-                        function( thisElement ){
-                            emailField = thisElement;
-                        } 
-                    }
-                    label="Email Address" required={false}/>
-                </FormControl>
-
-                <FormControl fullWidth sx={{ mb: 2 }}>
-                    <TextField 
-                    inputRef={ 
-                        function( thisElement ){
-                            passwordField = thisElement;
-                        } 
-                    }
-                    type="password"
-                    label="Password" required={true}  />
-                </FormControl>
-            </Box>
-
-            <Box mt={4} mb={4}>
-
-                <Typography component="p" variant="body1" gutterBottom>
-                    Update your profile picture (optional)
-                </Typography>
-
-                <br/>
-
-                <Button size="small" variant="contained" component="label">
-                    Upload
-                    <input 
-                        ref={function(thisElement){ avatarField = thisElement }} 
-                        onClick={attachFile}
-                        onChange={attachFile}
-                        accept="image" 
-                        multiple type="file" 
-                    />
-                </Button>
-
-            </Box>
-
-
-            <Box display="flex">
-                
-                {
-                    formState !== "loading" &&
-                    <Button onClick={updateHandler} size="large" variant="contained">Update profile</Button>
-                }
-                
-                {
-                    formState === "loading" &&
-                    <CircularProgress />
-                }
-            </Box>
-
-            <Box mt={2}>
-
-                { 
-                    formState === "client error" &&
-                    <Alert severity="error">
-                        <ul>
-                        {
-                            errorsState.map(addListItem)
+                        <Button size="small" variant="contained" component="label">
+                            Upload picture
+                            <input 
+                                ref={function(thisElement){ avatarField = thisElement }} 
+                                onClick={attachFile}
+                                onChange={attachFile}
+                                hidden accept="image/*" 
+                                multiple type="file" 
+                            />
+                        </Button>
+                    </Box>
+                </Box>
+                <Box mt={4} mb={2}>
+                    <FormControl fullWidth sx={{ mb: 2 }}>
+                        <TextField 
+                        inputRef={ 
+                            function( thisElement ){
+                                firstNameField = thisElement;
+                            } 
                         }
-                        </ul>
-                    </Alert>
-                }
+                        label="Firstname" 
+                        required={true}
+                        defaultValue={userDetails.firstName}
+                        />
+                    </FormControl>
 
-                { 
-                    formState === "backend error" &&
-                    <Alert severity="error">
-                        <ul>
-                        {
-                            errorsState.map(addListItem)
+                    <FormControl fullWidth sx={{ mb: 2 }}>
+                        <TextField 
+                        inputRef={ 
+                            function( thisElement ){
+                                lastNameField = thisElement;
+                            } 
                         }
-                        </ul>
-                    </Alert>
-                }
+                        label="Lastname" 
+                        required={true}
+                        defaultValue={userDetails.lastName}
+                        />
+                    </FormControl>
 
-                {
-                    formState === "success" &&
-                    <Alert severity="success">
-                        You have registered successfully!
-                    </Alert>
-                }
-            </Box>
-        </Container>
-    );
-  } else {
-    return <p>Loading...</p>;
-  }
+                    <FormControl fullWidth sx={{ mb: 2 }}>
+                        <TextField 
+                        inputRef={ 
+                            function( thisElement ){
+                                emailField = thisElement;
+                            } 
+                        }
+                        label="Email" 
+                        required={true}
+                        defaultValue={userDetails.email}
+                        />
+                    </FormControl>
+
+                    <FormControl fullWidth sx={{ mb: 2 }}>
+                        <TextField 
+                        disabled={!changePassword} 
+                        inputRef={ 
+                            function( thisElement ){
+                                passwordField = thisElement;
+                            } 
+                        }
+                        label="Password" 
+                        />
+                    </FormControl>
+                    <Checkbox 
+                    onChange={togglePasswordChange}
+                    inputRef={function(thisElement){passwordRadio = thisElement}} /> Change password?
+                </Box>
+
+                <Box display="flex">
+                    
+                    {
+                        formState !== "loading" &&
+                        <Button onClick={update} size="large" variant="contained">Update profile</Button>
+                    }
+                    
+                    {
+                        formState === "loading" &&
+                        <CircularProgress />
+                    }
+                </Box>
+
+                <Box mt={2}>
+
+                    { 
+                        formState === "client error" &&
+                        <Alert severity="error">
+                            <ul>
+                            {
+                                errorsState.map(addListItem)
+                            }
+                            </ul>
+                        </Alert>
+                    }
+
+                    {
+                        formState === "success" &&
+                        <Alert severity="success">
+                            You have logged in successfully!
+                        </Alert>
+                    }
+                </Box>
+            </Container>
+        )
+    } else {
+        return(
+            <p>Loading...</p>
+        )
+    }
 }
 
 export default ProfileScreen;
